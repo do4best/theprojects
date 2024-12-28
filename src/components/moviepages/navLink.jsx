ba37dccbd15115d097ec32fd70e3517c1e3b4636
Meer Afzal <do4best@gmail.com>
2024-12-28 19:24:37 +0500

    try to link the page

--- src/components/moviepages/navLink.jsx
+++ src/components/moviepages/navLink.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from "styled-components";
-import logo from '../netlogo.png';
-import {BrowserRouter, Route, Routes,Link} from "react-router-dom";
+import logo from './bannes.jpeg';
+import {signOut,onAuthStateChanged} from 'firebase/auth';
+import {firebaseAuth} from "../store/utils/firebase-config.js";
+import {BrowserRouter, Route, Routes,Link,useNavigate} from "react-router-dom";
 import {AiOutlineLogout} from "react-icons/ai";
 function NavLink(props) {
     const showLink = [
@@ -10,12 +12,16 @@ function NavLink(props) {
         {name : "My List", link : '/mylist'},
         {name : "Movies", link : '/movies'},
     ]
+    const navigate = useNavigate();
+    onAuthStateChanged(firebaseAuth,(currenUser)=>{
+        if(!currenUser)navigate("/login")
+    })
     return (
         <>
         <NaveContainer>
             <div className="leftside">
 <div className="logo">
-    {/*<img src={logo} alt="Logo"/>*/}
+    <img src={logo} alt="Logo" className={"bg-contain"}/>
 </div>
                 <ul className="links">
                     { showLink.map(({name,link})=>{
@@ -28,8 +34,8 @@ function NavLink(props) {
                 </ul>
             </div>
 <div className="rightside">
-    <button>
-        <AiOutlineLogout/>
+    <button onClick={()=>signOut(firebaseAuth)}>
+        <AiOutlineLogout size={22}/>
     </button>
 </div>
         </NaveContainer>
@@ -37,5 +43,39 @@ function NavLink(props) {
     );
 }
 const NaveContainer = styled.div`
+  
+    background-color: black;
+    display: flex;
+    flex-direction: row;
+    align-items: start;
+    justify-content: flex-start;
+    .logo{
+   height: 150px;
+        width: 400px;
+    }
+    .links{
+        display: flex;
+        padding: 50px;
+        flex-direction: row;
+        align-items: flex-start;
+        justify-content:start;
+        position: absolute;
+        top: 5px;
+        margin-top: 10px;
+        gap: 15px;
+        margin-left: 400px;
+        font-size: 18px;
+        color: white;
+        text-decoration: none;
+        list-style: none;
+    }
+    .rightside{
+        position: absolute;
+        right: 2px;
+        margin-top: 20px;
+        padding: 20px;
+       color: red;
+        
+    }
 `
 export default NavLink;
