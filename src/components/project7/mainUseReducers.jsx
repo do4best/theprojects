d79558a2de4325cd5460f24d1e26633e3c20af0b
Meer Afzal <do4best@gmail.com>
2025-01-14 23:10:24 +0500

    use reducer example

---
+++ src/components/project7/mainUseReducers.jsx
@@ -0,0 +1,25 @@
+import React, {useReducer} from 'react';
+function reducer(state,action){
+    switch (action.type){
+        case 'increment':
+            return state + 1;
+        case 'decrement':
+            return  state - 1;
+        default:
+            throw new Error("No such action exist")
+    }
+
+}
+function MainUseReducers(props) {
+    const [state,dispatch] = useReducer(reducer,0)
+    return (
+        <>
+        <h1>{state}</h1>
+            <button onClick={()=>dispatch({type:'increment'})}>+</button>
+            <button onClick={()=>dispatch({type:'decrement'})}>-</button>
+
+        </>
+    );
+}
+
+export default MainUseReducers;
