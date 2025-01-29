59f3e28326b8b03d063ef7b62fbb9ff52d339558
Meer Afzal <do4best@gmail.com>
2025-01-29 23:37:33 +0500

    the project is completed with hundered percent up and running

--- src/components/project8/todoList.jsx
+++ src/components/project8/todoList.jsx
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import TodoForm from "./todoForm.jsx";
 import TodoItems from "./todoItems.jsx";
 
 function TodoList(props) {
     const [todos,setTodos] = useState([])
     const [editData, setEditData] = useState(null)
+    const [searchParams, setSearchParams] = useState("")
 const handelmodifytodo =(todosone)=>{
         const newtodos = [...todos];
         const indexoflatesttodos = newtodos.findIndex((item)=>item.id === todosone.id);
@@ -17,21 +18,40 @@ newtodos[indexoflatesttodos] ={
 }
         }
         setTodos(newtodos)
-    console.log(todos)
+   localStorage.setItem('todoList',JSON.stringify(newtodos))
 }
 const getEdittodoDate=(edit)=>{
 setEditData(edit)
 }
 const  handelDelete=(iddelete)=>{
-        let deleteid = [...todos];
-        deleteid = deleteid.filter((item)=>item.id!==iddelete);
-        setTodos(deleteid)
-}
+    let deleteid = [...todos];
+    deleteid = deleteid.filter((item)=>item.id!==iddelete);
+    setTodos(deleteid)
+    localStorage.setItem('todoList',JSON.stringify(deleteid))
 
+}
+    useEffect(() => {
+        const extractLocalStorage = JSON.parse(localStorage.getItem('todoList'));
+        if(extractLocalStorage && extractLocalStorage.length){
+            setTodos(extractLocalStorage)
+        }
+    }, []);
+    const handelOnSearch=(e)=>{
+        const {value} = e.target;
+        setSearchParams(value.toLowerCase())
+
+    }
+
+
+    const filterTodos = todos && todos.length ? todos.filter(item => item.text.toLowerCase().includes(searchParams)) : []
     return (
         <>
+            <div>
+                <h3 className={"text-3xl m-3"}>Search Todos</h3>
+                <input type="text" name={"search"} placeholder={"SearchTodos"} className={"input input-bordered w-full   "} onChange={handelOnSearch} value={searchParams}/>
+            </div>
         <TodoForm editData={editData} getNewlyCreatedItem = {handelmodifytodo}/>
-        <TodoItems getIDtobeDeleted={handelDelete} getEdittodoDate={getEdittodoDate} todos={todos}/> </>
+        <TodoItems getIDtobeDeleted={handelDelete} getEdittodoDate={getEdittodoDate} todos={filterTodos}/> </>
     );
 }
 
