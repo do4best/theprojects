fb5ac22588166a33153390b2eda7e27649e28f5f
Meer Afzal <do4best@gmail.com>
2025-02-09 23:55:49 +0500

    finding the navebar for better understanding

---
+++ src/components/a_mantine_Project/mainMantine.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Title} from "@mantine/core";
+
+function MainMantine(props) {
+    return (
+        <>
+
+        <Title order={5} align={"center"}>Hello Friend</Title>
+
+        </>
+    );
+}
+
+export default MainMantine;
